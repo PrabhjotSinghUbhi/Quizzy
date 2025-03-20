5cca2e0a824aeb482c109987caa370be2e679588
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-20 23:38:07 +0530

    Basic UI made responsive for web and mobile

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Div that contains the question.
+const questionDiv = document.querySelector("section div")
+
+//Question number.
+const qNo = document.querySelector("#qNo")
+
+//The Options.
+const options = document.querySelectorAll("li article div")
